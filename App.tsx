b7b7b7bdae75dfbc34a1b1425e4cb40c7d12a5a7
hadar.amsalem <73754433+hadaramsi@users.noreply.github.com>
2023-01-12 15:36:26 +0200

    first commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,91 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { FC, useState } from 'react';
+import { StatusBar, StyleSheet, Text, View, Image, TouchableOpacity, Button, Alert, TextInput } from 'react-native';
 
-export default function App() {
+
+const Brick: FC<{ onClick: () => void, getCurrentPlayer: () => number }> = (props) => {
+  const [player, setPlayer] = useState(0)
+  const onClick = () => {
+    setPlayer(props.getCurrentPlayer())
+    props.onClick()
+  }
+  const getBackground = () => {
+    if (player == 0) {
+      return 'white'
+    } else if (player == 1) {
+      return 'red'
+    }
+    return 'green'
+  }
+  return (
+    <View style={styles.brick}>
+      <TouchableOpacity style={[styles.button, { backgroundColor: getBackground() }]} onPress={onClick}>
+      </TouchableOpacity>
+    </View>
+  )
+}
+const App: FC = () => {
+  // 0: not selected, 1: 'x', 2: 'o'
+  var turn = 1
+
+  const getCurrentPlayer = () => {
+    return turn
+  }
+  const onBrickClick = () => {
+    console.log('onBrickClick')
+    if (turn == 1) {
+      turn = 2
+    } else {
+      turn = 1
+    }
+  }
+
+  console.log("My app is running")
+
+  const onPressCallback = () => {
+    console.log("button was pressed")
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={styles.row}>
+        <Brick onClick={onBrickClick} getCurrentPlayer={getCurrentPlayer}></Brick>
+        <Brick onClick={onBrickClick} getCurrentPlayer={getCurrentPlayer}></Brick>
+        <Brick onClick={onBrickClick} getCurrentPlayer={getCurrentPlayer}></Brick>
+      </View>
+      <View style={styles.row}>
+        <Brick onClick={onBrickClick} getCurrentPlayer={getCurrentPlayer}></Brick>
+        <Brick onClick={onBrickClick} getCurrentPlayer={getCurrentPlayer}></Brick>
+        <Brick onClick={onBrickClick} getCurrentPlayer={getCurrentPlayer}></Brick>
+      </View>
+      <View style={styles.row}>
+        <Brick onClick={onBrickClick} getCurrentPlayer={getCurrentPlayer}></Brick>
+        <Brick onClick={onBrickClick} getCurrentPlayer={getCurrentPlayer}></Brick>
+        <Brick onClick={onBrickClick} getCurrentPlayer={getCurrentPlayer}></Brick>
+      </View>
+    </View >
   );
 }
 
+
 const styles = StyleSheet.create({
   container: {
+    marginTop: StatusBar.currentHeight,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'grey',
+  },
+  row: {
+    flexDirection: 'row',
+    backgroundColor: 'blue',
+  },
+  brick: {
+    flex: 1,
+    backgroundColor: 'white',
+    margin: 5,
+    aspectRatio: 1,
+  },
+  button: {
+    flex: 1,
   },
 });
+
+export default App
